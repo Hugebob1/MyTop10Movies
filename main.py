ebf7c4a18964e2929d1dfb476e75fdd8976bee74
Hugebob1 <01197882@pw.edu.pl>
2025-07-04 13:51:05 +0200

    starting point

---
+++ main.py
@@ -0,0 +1,62 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///top10Movies-collection.db"
+Bootstrap5(app)
+# CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+db = SQLAlchemy(model_class=Base)
+# Initialise the app with the extension
+db.init_app(app)
+# CREATE TABLE
+
+class Movie(db.Model):
+    __tablename__ = 'movies'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80), nullable=False, unique=True)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return '<Movie %r>' % self.title
+
+with app.app_context():
+    db.create_all()
+
+# with app.app_context():
+#     new_movie = Movie(
+#         title="Phone Booth",
+#         year=2002,
+#         description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#         rating=7.3,
+#         ranking=10,
+#         review="My favourite character was the caller.",
+#         img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+#     )
+#     db.session.add(new_movie)
+#     db.session.commit()
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
